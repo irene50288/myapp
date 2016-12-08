3a010640874d9957a07b150941298eca66a518fc
Iryna Koroliuk <ikoroliuk@google.com>
2016-12-08 15:05:14 +0100

    try to post to /upload route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res) {
+  res.send('post request to homepage');
+});
+
 module.exports = router;
