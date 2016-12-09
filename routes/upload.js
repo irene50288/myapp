56d23b2404c3fafc4a7ce16e3138ea41db5bfda2
Iryna Koroliuk <ikoroliuk@google.com>
2016-12-09 12:02:00 +0100

    google-cloud create bucket

--- routes/upload.js
+++ routes/upload.js
@@ -1,18 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var gcloud = require('gcloud');
 
-var config = {};
+var config = {
+    projectId: 'fleet-tensor-152008'
+};
+var gcs = gcloud.storage(config);
+
+gcs.createBucket('my-new-bucket', function(err, bucket) {
+  if (!err) {
+    // "my-new-bucket" was successfully created.
+  }
+});
+
+var bucket = gcs.bucket('my-existing-bucket');
+
 /* POST to /upload */
 router.post('/', function (req, res) {
 
-    var gcloud = require('gcloud')(config);
-    var gcs = gcloud.storage();
-    var bucket = gcs.bucket(config.storageBucket);
 
     if (!req.files) {
         res.send('No files were uploaded.');
         return;
     }
+
+  bucket.upload('/photos/zoo/zebra.jpg', function(err, file) {
+    if (!err) {
+      // "zebra.jpg" is now in your bucket.
+    }
+  });
 
     res.send('File uploaded!');
 
