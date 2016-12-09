0bfbcf98d5190bdbb9b4bea73482763c4d1d3410
Iryna Koroliuk <ikoroliuk@google.com>
2016-12-09 16:01:07 +0100

    multer added

--- routes/upload.js
+++ routes/upload.js
@@ -3,14 +3,18 @@ var router = express.Router();
 var images = require('../lib/images');
 
 /* POST to /upload */
-router.post('/', images.multer.single('image'), images.sendUploadToGCS, function(req, res, next) {
+router.post('/', images.multer.single('sampleFile'), images.sendUploadToGCS, function(req, res, next) {
   var data = req.body;
 
   if (req.file && req.file.cloudStoragePublicUrl) {
     data.imageUrl = req.file.cloudStoragePublicUrl;
+    res.send('File uploaded!');
+  } else {
+    console.log(req.body);
+    res.send('failed');
   }
 
-  res.send('File uploaded!');
+
 
 });
 
