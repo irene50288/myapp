40dfa592799bb1ca1e4ac565ec342957cf5d94bd
Iryna Koroliuk <ikoroliuk@google.com>
2016-12-21 16:53:57 +0100

    add setting to choose colorpicker

--- public/javascripts/client_upload.js
+++ public/javascripts/client_upload.js
@@ -17,9 +17,8 @@ var uploadFile = function(el) {
   var tStart = performance.now();
   var file = el.files[0];
 
-  var $picker = document.getElementById("colorPicker");
-  var c = $picker.children[1].children[0];
-  var ctx = c.getContext("2d");
+  // var $picker = document.getElementById("primaryFgColorPicker");
+
 
 
   var xhr = new XMLHttpRequest();
@@ -28,9 +27,10 @@ var uploadFile = function(el) {
       // console.log(xhr.responseText);
       img.src = xhr.responseText;
       imageContainer.appendChild(img);
-      img.onload = function() {
-        ctx.drawImage(img, 10, 10);
-      };
+      // img.onload = function() {
+      //   console.log(ctx);
+      //   ctx.drawImage(img, 10, 10);
+      // };
     }
   }
 
@@ -38,5 +38,56 @@ var uploadFile = function(el) {
   formData.append('theFile', file);
   xhr.open('POST', 'http://localhost:3000');
   xhr.send(formData);
+
+};
+
+var toggleUploadImageField = function(el){
+  var fileWrapper = document.getElementById('fileForPicker');
+  if (el.checked) {
+    fileWrapper.style.display = 'block';
+  } else {
+    fileWrapper.style.display = 'none';
+  }
+};
+
+var toggleCustomColorPicker = function(el, id){
+  var nativePicker = document.getElementById(id + 'Input');
+  var picker = document.getElementById(id);
+  var img = document.getElementById('imageContainer').children[0];
+  if (el.checked) {
+    picker.style.display = 'block';
+    nativePicker.style.display = 'none';
+    tinycolorpicker(picker);
+    var c = picker.children[1].children[0];
+    var ctx = c.getContext("2d");
+    ctx.drawImage(img, 10, 10);
+  } else {
+    picker.style.display = 'none';
+    nativePicker.style.display = 'block';
+  }
+};
+var generateColorPicker = function(id){
+
+  var container = document.createElement('div');
+  container.id = id;
+  container.className = 'colorPicker';
+  var color = document.createElement('a');
+  color.className = 'color';
+  var colorInner = document.createElement('div');
+  colorInner.className = 'colorInner';
+  color.appendChild(colorInner);
+  var track = document.createElement('div');
+  track.className = 'track';
+  var input = document.createElement('input');
+  input.type = 'hidden';
+  input.name = 'id';
+  input.className = 'colorInput';
+  container.appendChild(color);
+  container.appendChild(track);
+  container.appendChild(input);
+
+
+
+
 
 };
