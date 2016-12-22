0da8cc6d5cda2f2afb11fc4819248f4e3ce03b0a
Iryna Koroliuk <ikoroliuk@google.com>
2016-12-22 16:33:49 +0100

    position colorpicker over image, show selected color

--- public/javascripts/client_upload.js
+++ public/javascripts/client_upload.js
@@ -11,34 +11,26 @@ firebase.initializeApp(config);
 var uploadFile = function(el) {
   var imageContainer = document.getElementById('imageContainer');
   var img = document.createElement('img');
-  // img.crossOrigin = "Anonymous";
-  img.width = 150;
-  img.height = 150;
+  // img.width = 150;
+  // img.height = 150;
   var tStart = performance.now();
   var file = el.files[0];
-
-  // var $picker = document.getElementById("primaryFgColorPicker");
-
-
-
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (xhr.readyState == XMLHttpRequest.DONE) {
-      // console.log(xhr.responseText);
       img.src = xhr.responseText;
       imageContainer.appendChild(img);
-      // img.onload = function() {
-      //   console.log(ctx);
-      //   ctx.drawImage(img, 10, 10);
-      // };
+      img.onload = function(){
+        var rect = img.getBoundingClientRect();
+        console.log(rect.top, rect.right, rect.bottom, rect.left);
+        generateColorPicker('primaryBgColor', img);
+      };
     }
-  }
-
+  };
   var formData = new FormData();
   formData.append('theFile', file);
   xhr.open('POST', 'http://localhost:3000');
   xhr.send(formData);
-
 };
 
 var toggleUploadImageField = function(el){
@@ -66,10 +58,9 @@ var toggleCustomColorPicker = function(el, id){
     nativePicker.style.display = 'block';
   }
 };
-var generateColorPicker = function(id){
-
+var generateColorPicker = function(id, image){
   var container = document.createElement('div');
-  container.id = id;
+  container.id = id + 'Picker';
   container.className = 'colorPicker';
   var color = document.createElement('a');
   color.className = 'color';
@@ -80,14 +71,42 @@ var generateColorPicker = function(id){
   track.className = 'track';
   var input = document.createElement('input');
   input.type = 'hidden';
-  input.name = 'id';
+  input.name = id;
   input.className = 'colorInput';
   container.appendChild(color);
   container.appendChild(track);
   container.appendChild(input);
+  var place = document.getElementById(id + 'Wrapper');
+  place.innerHTML = '';
+  place.appendChild(container);
+  // init color picker
+  var picker = tinycolorpicker(container);
+
+  // put the image to canvas
+  var c = container.children[1].children[0];
+  var ctx = c.getContext("2d");
+  ctx.canvas.width = image.width;
+  ctx.canvas.height = image.height;
+  ctx.drawImage(image, 0, 0);
+  var imageRect = image.getBoundingClientRect();
+  container.style.left = imageRect.left + 'px';
+  container.style.top = imageRect.top + 'px';
+  container.style.position = 'absolute';
+  console.log(image.width, image.height);
+
+  container.addEventListener('change',function(){
+    var destinationInput = document.getElementById(id + 'Input');
+    destinationInput.value = input.value;
+  });
 
 
+};
 
-
-
+var generateNativeColorPicker = function(id){
+  var input = document.createElement('input');
+  input.type = 'color';
+  input.name = 'id';
+  var place = document.getElementById(id + 'Wrapper');
+  place.innerHTML = '';
+  place.appendChild(input);
 };
