eac28942f579c7fa707a3440440da708a8276416
Iryna Koroliuk <ikoroliuk@google.com>
2017-01-09 16:58:16 +0100

    edit

--- routes/edit.js
+++ routes/edit.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var request = require('request');
+var images = require('../lib/images');
 
 
 var parseFileNames = function(obj){
@@ -18,15 +18,12 @@ var parseFileNames = function(obj){
 
 /* GET home page. */
 router.get('/:bucketId', function(req, res) {
-  request('https://storage.googleapis.com/'+ req.params.bucketId+'/result.txt', function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var configObj = JSON.parse(body);
-      configObj.bucketName = bucketId;
-      parseFileNames(configObj);
-      res.render('index', { configObj: configObj });
-    } else {
-      console.log(error);
-    }
+  images.getConfigJson(req.params.bucketId).then(function(configObj){
+    configObj.bucketName = req.params.bucketId;
+    parseFileNames(configObj);
+    res.render('index', { configObj: configObj });
+  }).catch(function(err){
+    console.log(err);
   });
 });
 
